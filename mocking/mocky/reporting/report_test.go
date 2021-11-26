4d70cc452f5f313907de10192587cff7e539a504
David MacNeil <david.macneil@forgerock.com>
2021-11-26 15:15:59 +0000

    test outline

--- mocking/mocky/reporting/report_test.go
+++ mocking/mocky/reporting/report_test.go
@@ -3,5 +3,9 @@ package reporting
 import (
 	"testing"
 
-	"github.com/fr-dam/various/mocky/mocks"
+	"github.com/fr-dam/various/mocking/mocky/reporting/mocks"
 )
+
+func Test_Write(t *testing.T) {
+	writer := &mocks.ReportWriter{}
+}
